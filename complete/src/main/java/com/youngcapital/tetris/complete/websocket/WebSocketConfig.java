65eef004ecf0548cecc6835969bec59467380d94
Wouter Remijn <wouter.remijn@gmail.com>
2017-06-22 14:41:08 +0200

    placed websocketconfig in websocket package

--- complete/src/main/java/com/youngcapital/tetris/complete/websocket/WebSocketConfig.java
+++ complete/src/main/java/com/youngcapital/tetris/complete/websocket/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.youngcapital.tetris.complete;
+package com.youngcapital.tetris.complete.websocket;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
