20946b8a460dfda30f12fcbe5bf8ae407c15b1d1
Wouter Remijn <wouter.remijn@gmail.com>
2017-06-23 14:30:24 +0200

    error demo

--- complete/src/main/java/com/youngcapital/tetris/complete/TetrisController.java
+++ complete/src/main/java/com/youngcapital/tetris/complete/TetrisController.java
@@ -12,17 +12,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.youngcapital.tetris.complete.websocket.ControlMessage;
 import com.youngcapital.tetris.complete.websocket.Greeting;
+import com.youngcapital.tetris.complete.websocket.MoveMessage;
 
 @Controller
 public class TetrisController {
 	
+	private boolean[][] grid;
+	private Point[] currentPositions;
+	
 	@Autowired
-	private SimpMessagingTemplate template;
+	public SimpMessagingTemplate template;
 	
 	@RequestMapping("/tetris")
 	public String createPage(Model model){
-		model.addAttribute("gridHeight", 19);
-		model.addAttribute("gridWidth", 9);
+		int gridHeight = 20;
+		int gridWidth = 10;
+		grid = new boolean[gridHeight][gridWidth];
+		model.addAttribute("gridHeight", gridHeight-1);
+		model.addAttribute("gridWidth", gridWidth-1);
 		return "tetris";
 	}
 	
@@ -39,13 +46,29 @@ public class TetrisController {
 		return new Greeting("Wrong Input, You Cheater!: " + message.getKeyboardCode());
 	}
 	
-	@MessageMapping("/init")	
-	public void initGreeting() {
-		test();
-		//return new Greeting(new Point[]{}, new Point[]{new Point(4,0)}, "red");
+	@MessageMapping("/init")
+	@SendTo("/tetris/init")
+	public Greeting initGreeting() {
+		currentPositions = createBlock();
+		return new Greeting(new Point[]{}, currentPositions, "red");
 	}
 	
-	public void test(){
-		template.convertAndSend("/tetris/init", new Greeting(new Point[]{}, new Point[]{new Point(4,0)}, "red"));
+	public Point[] createBlock(){
+		return new Point[]{new Point(4,0)};
+	}
+	
+	@MessageMapping("/move")
+	@SendTo("/tetris/move")
+	public Greeting moveGreeting(MoveMessage message) {
+		Point move = new Point(message.getX(), message.getY());
+		Point[] oldPositions = new Point[currentPositions.length];
+		for(int i = 0; i < oldPositions.length; i++){
+			oldPositions[i] = (Point) currentPositions[i].clone();
+			currentPositions[i].setLocation(currentPositions[i].getX() + message.getX(), currentPositions[i].getY() + message.getY());
+		}
+		
+		
+		
+		return new Greeting(oldPositions, currentPositions, "red");
 	}
 }
